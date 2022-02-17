fa542732f723f3a91afb8b8033c62fff09291e4e
Prince Anuragi <prince@uniris.io>
2022-02-18 00:58:32 +0530

    Some UI Improvements

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import React, { useState, useRef, useEffect } from 'react';
-import './App.css';
-
+import React, { useState, useRef, useEffect } from "react";
+import "./App.css";
 import { Graph, Node } from "./graph";
 import { ConstructInitGraph } from "./utils";
 
@@ -13,7 +12,7 @@ const drawLine = (ctx, x1, y1, x2, y2, color, width) => {
   ctx.lineWidth = width || 1;
   ctx.stroke();
   ctx.restore();
-}
+};
 
 const drawCircle = (ctx, x, y, r, color) => {
   ctx.beginPath();
@@ -21,7 +20,7 @@ const drawCircle = (ctx, x, y, r, color) => {
   ctx.fillStyle = color || "red";
   ctx.fill();
   ctx.save();
-}
+};
 
 const drawRectangle = (ctx, x, y, w, h, color, lineWidth) => {
   ctx.save();
@@ -32,7 +31,7 @@ const drawRectangle = (ctx, x, y, w, h, color, lineWidth) => {
   ctx.strokeStyle = "dotted";
   ctx.stroke();
   ctx.save();
-}
+};
 
 const drawRectangleSolid = (ctx, x, y, w, h, color) => {
   ctx.beginPath();
@@ -40,12 +39,13 @@ const drawRectangleSolid = (ctx, x, y, w, h, color) => {
   ctx.fillStyle = color || "gray";
   ctx.fill();
   ctx.save();
-}
-
+};
 
 const drawArrow = (ctx, p1, p2, size) => {
-  var angle = Math.atan2((p2.y - p1.y), (p2.x - p1.x));
-  var hyp = Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+  var angle = Math.atan2(p2.y - p1.y, p2.x - p1.x);
+  var hyp = Math.sqrt(
+    (p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y)
+  );
 
   ctx.save();
   ctx.translate(p1.x, p1.y);
@@ -59,7 +59,7 @@ const drawArrow = (ctx, p1, p2, size) => {
   ctx.stroke();
 
   // triangle
-  ctx.fillStyle = 'teal';
+  ctx.fillStyle = "teal";
   ctx.beginPath();
   ctx.lineTo(hyp - size, size);
   ctx.lineTo(hyp, 0);
@@ -67,13 +67,12 @@ const drawArrow = (ctx, p1, p2, size) => {
   ctx.fill();
 
   ctx.restore();
-}
+};
 
 const MIN_X_SCALE = 60;
 const MIN_Y_SCALE = 60;
 
 function App() {
-
   const [graph, setGraph] = useState(new Graph());
 
   const [pacman, setPacman] = useState(new Node(1, 1));
@@ -82,61 +81,141 @@ function App() {
   const canvasRef = useRef(null);
 
   useEffect(() => {
-
     let graph = ConstructInitGraph();
 
     setGraph(graph);
-
-  }, [])
+  }, []);
 
   const construct_grid = () => {
     if (graph) {
       const canvas = canvasRef.current;
-      const context = canvas.getContext('2d');
+      const context = canvas.getContext("2d");
       context.fillStyle = "#ccc";
       context.fillRect(0, 0, context.canvas.width, context.canvas.height);
       const vertexes = graph.getVertexes();
 
       vertexes.forEach((node) => {
         // console.log(node.x * 10, node.y * 10);
-        drawRectangle(context, node.x * MIN_X_SCALE, node.y * MIN_Y_SCALE, MIN_X_SCALE, MIN_Y_SCALE, "#e6e600");
-      })
+        drawRectangle(
+          context,
+          node.x * MIN_X_SCALE,
+          node.y * MIN_Y_SCALE,
+          MIN_X_SCALE,
+          MIN_Y_SCALE,
+          "#e6e600"
+        );
+      });
 
-      drawCircle(context, (pacman.x * MIN_X_SCALE) + 30, (pacman.y * MIN_Y_SCALE) + 30, 20, "#ffea00")
-      drawRectangleSolid(context, (food.x * MIN_X_SCALE) + 20, (food.y * MIN_Y_SCALE) + 20, 20, 20, "#E74C3C")
+      drawCircle(
+        context,
+        pacman.x * MIN_X_SCALE + 30,
+        pacman.y * MIN_Y_SCALE + 30,
+        20,
+        "#ffea00"
+      );
+      drawRectangleSolid(
+        context,
+        food.x * MIN_X_SCALE + 20,
+        food.y * MIN_Y_SCALE + 20,
+        20,
+        20,
+        "#E74C3C"
+      );
 
       const edges = graph.getEdges();
 
       const borderColor = "#0d47a1";
       const borderWidth = 5;
 
-      drawLine(context, 1 * MIN_X_SCALE, 1 * MIN_Y_SCALE, 5 * MIN_X_SCALE, 1 * MIN_Y_SCALE, borderColor, borderWidth);
-      drawLine(context, 1 * MIN_X_SCALE, 4 * MIN_Y_SCALE, 5 * MIN_X_SCALE, 4 * MIN_Y_SCALE, borderColor, borderWidth);
-      drawLine(context, 1 * MIN_X_SCALE, 1 * MIN_Y_SCALE, 1 * MIN_X_SCALE, 4 * MIN_Y_SCALE, borderColor, borderWidth);
-      drawLine(context, 5 * MIN_X_SCALE, 1 * MIN_Y_SCALE, 5 * MIN_X_SCALE, 4 * MIN_Y_SCALE, borderColor, borderWidth);
+      drawLine(
+        context,
+        1 * MIN_X_SCALE,
+        1 * MIN_Y_SCALE,
+        5 * MIN_X_SCALE,
+        1 * MIN_Y_SCALE,
+        borderColor,
+        borderWidth
+      );
+      drawLine(
+        context,
+        1 * MIN_X_SCALE,
+        4 * MIN_Y_SCALE,
+        5 * MIN_X_SCALE,
+        4 * MIN_Y_SCALE,
+        borderColor,
+        borderWidth
+      );
+      drawLine(
+        context,
+        1 * MIN_X_SCALE,
+        1 * MIN_Y_SCALE,
+        1 * MIN_X_SCALE,
+        4 * MIN_Y_SCALE,
+        borderColor,
+        borderWidth
+      );
+      drawLine(
+        context,
+        5 * MIN_X_SCALE,
+        1 * MIN_Y_SCALE,
+        5 * MIN_X_SCALE,
+        4 * MIN_Y_SCALE,
+        borderColor,
+        borderWidth
+      );
 
-      drawLine(context, 2 * MIN_X_SCALE, 2 * MIN_Y_SCALE, 2 * MIN_X_SCALE, 3 * MIN_Y_SCALE, borderColor, borderWidth);
-      drawLine(context, 2 * MIN_X_SCALE, 3 * MIN_Y_SCALE, 4 * MIN_X_SCALE, 3 * MIN_Y_SCALE, borderColor, borderWidth);
-      drawLine(context, 4 * MIN_X_SCALE, 2 * MIN_Y_SCALE, 4 * MIN_X_SCALE, 3 * MIN_Y_SCALE, borderColor, borderWidth);
-      drawLine(context, 4 * MIN_X_SCALE, 2 * MIN_Y_SCALE, 3 * MIN_X_SCALE, 2 * MIN_Y_SCALE, borderColor, borderWidth);
+      drawLine(
+        context,
+        2 * MIN_X_SCALE,
+        2 * MIN_Y_SCALE,
+        2 * MIN_X_SCALE,
+        3 * MIN_Y_SCALE,
+        borderColor,
+        borderWidth
+      );
+      drawLine(
+        context,
+        2 * MIN_X_SCALE,
+        3 * MIN_Y_SCALE,
+        4 * MIN_X_SCALE,
+        3 * MIN_Y_SCALE,
+        borderColor,
+        borderWidth
+      );
+      drawLine(
+        context,
+        4 * MIN_X_SCALE,
+        2 * MIN_Y_SCALE,
+        4 * MIN_X_SCALE,
+        3 * MIN_Y_SCALE,
+        borderColor,
+        borderWidth
+      );
+      drawLine(
+        context,
+        4 * MIN_X_SCALE,
+        2 * MIN_Y_SCALE,
+        3 * MIN_X_SCALE,
+        2 * MIN_Y_SCALE,
+        borderColor,
+        borderWidth
+      );
 
       // edges.forEach((edge) => {
-      //   drawArrow(context, 
+      //   drawArrow(context,
       //     new Node(
-      //       (edge.nodeA.x * MIN_X_SCALE) + 30 + Math.floor(Math.random() * 10), 
+      //       (edge.nodeA.x * MIN_X_SCALE) + 30 + Math.floor(Math.random() * 10),
       //       (edge.nodeA.y * MIN_Y_SCALE) + 30 + Math.floor(Math.random() * 10)
-      //     ), 
+      //     ),
       //     new Node(
-      //       (edge.nodeB.x * MIN_X_SCALE) + 30 + Math.floor(Math.random() * 10), 
+      //       (edge.nodeB.x * MIN_X_SCALE) + 30 + Math.floor(Math.random() * 10),
       //       (edge.nodeB.y * MIN_Y_SCALE) + 30 + Math.floor(Math.random() * 10)
-      //     ), 
+      //     ),
       //     10
       //   );
       // })
     }
-  }
-
-
+  };
 
   const construct_graph = () => {
     if (graph) {
@@ -144,17 +223,16 @@ function App() {
     } else {
       console.log("graph Not Found");
     }
-  }
+  };
 
   useEffect(() => {
-    construct_graph()
-  })
+    construct_graph();
+  });
 
-  const get_current_neighbours = current_vertex => {
-    
+  const get_current_neighbours = (current_vertex) => {
     let neighbours = new Array();
 
-    if(graph) {
+    if (graph) {
       let edges = graph.getEdges();
       for (let i = 0; i < edges.length; ++i) {
         let a = edges[i].nodeA;
@@ -164,18 +242,17 @@ function App() {
         }
       }
     } else {
-      throw "Graph Not Found"
+      throw "Graph Not Found";
     }
 
     return neighbours.reverse();
-  }
+  };
 
   const sleep = (ms) => {
-      return new Promise(resolve => setTimeout(resolve, ms));
-  }
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  };
 
   const depth_first_search = async (start, goal) => {
-
     // console.log(start, goal);
 
     let visited = new Set();
@@ -185,11 +262,10 @@ function App() {
     fringe.push(start);
     visited.add(start.x.toString() + start.y.toString());
 
-
     // Shift is POP FRONT
     let current_vertex = fringe.shift();
 
-    while(current_vertex != undefined) {
+    while (current_vertex != undefined) {
       history.push(current_vertex);
 
       // GUI Handle to show PACMAN Position
@@ -203,35 +279,37 @@ function App() {
       }
 
       let current_neighbours = get_current_neighbours(current_vertex);
-      
-      for (let i =0; i < current_neighbours.length;  ++i) {
 
-        if (!visited.has(current_neighbours[i].x.toString() + current_neighbours[i].y.toString())) {
-
-          visited.add(current_neighbours[i].x.toString() + current_neighbours[i].y.toString());
+      for (let i = 0; i < current_neighbours.length; ++i) {
+        if (
+          !visited.has(
+            current_neighbours[i].x.toString() +
+              current_neighbours[i].y.toString()
+          )
+        ) {
+          visited.add(
+            current_neighbours[i].x.toString() +
+              current_neighbours[i].y.toString()
+          );
 
           // Unshift Does a Push Front
           fringe.unshift(current_neighbours[i]);
-
         }
       }
 
       // Shift Does a push front
       current_vertex = fringe.shift();
-
     }
 
     return null;
-    
-  }
+  };
 
   const uniform_cost_search = (start, goal) => {
-
     let answer = new Array();
     let queue = new Array();
 
     for (let i = 0; i < goal.length; ++i) {
-      answer.push(10**8);
+      answer.push(10 ** 8);
     }
 
     // Fill the Answers with MAX Value
@@ -245,7 +323,7 @@ function App() {
 
     let count = 0;
 
-    while(queue.length > 0) {
+    while (queue.length > 0) {
       queue = queue.sort();
 
       let p = queue.pop();
@@ -256,21 +334,21 @@ function App() {
       p[0] *= -1;
 
       if (goal.includes(p[1])) {
+        let index = goal.findIndex((e) => {
+          e.x === p[1].x && e.y === p[1].y;
+        });
 
-        let index = goal.findIndex((e) => {e.x === p[1].x && e.y === p[1].y})
-
-        if (answer[index] === 10**8) {
-          count += 1
+        if (answer[index] === 10 ** 8) {
+          count += 1;
         }
 
         if (answer[index] > p[0]) {
-          answer[index] = p[0]
+          answer[index] = p[0];
         }
 
-        queue.pop()
+        queue.pop();
 
-
-        queue = queue.sort()
+        queue = queue.sort();
 
         if (count === goal.length) {
           return answer;
@@ -279,10 +357,10 @@ function App() {
 
       if (!visited.has(p[1])) {
         let cur_n = get_current_neighbours(p[1]);
-        for(let i = 0; i < cur_n.length; ++i) {
+        for (let i = 0; i < cur_n.length; ++i) {
           let queue_elem = new Array();
           let cost = graph.findEdgePair(p[1], cur_n[i]);
-          
+
           queue_elem.push(p[0] + cost - 1);
           queue_elem.push(cur_n[i]);
           queue.push(queue_elem);
@@ -290,57 +368,101 @@ function App() {
       }
 
       visited.set(p[1], 1);
-
     }
 
     console.log(answer);
     return answer;
-
-
-  }
+  };
 
   let goal_node = new Array();
   goal_node.push(new Node(3, 2));
 
   return (
-    <main>
-      {
-        graph ? <p></p> : <p>{"Graph is Not Loaded!!!!!!!!!!!!!"}</p>
-      }
-      <canvas ref={canvasRef} width={500} height={300} />
-      <div>
-        <div>
-          <p>Pacman X</p>
-          <input 
-            type={"number"} 
-            placeholder="Pacman X" 
-            onChange={(e) => setPacman(new Node(e.target.value, pacman.y))} 
-          />
-          <p>Pacman Y</p>
-          <input 
-            type={"number"} 
-            placeholder="Pacman Y" 
-            onChange={(e) => setPacman(new Node(pacman.x, e.target.value))} 
-          />
+    <div>
+      <nav class="navbar navbar-light bg-light">
+        <div class="container-fluid">
+          <span class="navbar-brand mb-0 h1">Pacman using DFS and UCS</span>
         </div>
+      </nav>
+      <main>
+        <canvas ref={canvasRef} width={350} height={300} />
         <div>
-          <p>Food X</p>
-          <input 
-            type={"number"} 
-            placeholder="Food X"
-            onChange={(e) => setFood(new Node(e.target.value, food.y))}
-          />
-          <p>Food Y</p>
-          <input 
-            type={"number"} 
-            placeholder="Food Y" 
-            onChange={(e) => setFood(new Node(food.x, e.target.value))} 
-          />
+          {graph ? <p></p> : <p>{"Graph is Not Loaded!!!!!!!!!!!!!"}</p>}
+          <div>
+            <div class="input-group input-group-sm mb-3">
+              <span class="input-group-text" id="inputGroup-sizing-sm">
+                Pacman X
+              </span>
+              <input
+                type="number"
+                class="form-control"
+                aria-label="Sizing example input"
+                aria-describedby="inputGroup-sizing-sm"
+                onChange={(e) => setPacman(new Node(e.target.value, pacman.y))}
+              />
+            </div>
+            <div class="input-group input-group-sm mb-3">
+              <span class="input-group-text" id="inputGroup-sizing-sm">
+                Pacman Y
+              </span>
+              <input
+                type="number"
+                class="form-control"
+                aria-label="Sizing example input"
+                aria-describedby="inputGroup-sizing-sm"
+                onChange={(e) => setPacman(new Node(pacman.x, e.target.value))}
+              />
+            </div>
+          </div>
+          <div>
+            <div class="input-group input-group-sm mb-3">
+              <span class="input-group-text" id="inputGroup-sizing-sm">
+                Food X
+              </span>
+              <input
+                type="number"
+                class="form-control"
+                aria-label="Sizing example input"
+                aria-describedby="inputGroup-sizing-sm"
+                onChange={(e) => setFood(new Node(e.target.value, food.y))}
+              />
+            </div>
+            <div class="input-group input-group-sm mb-3">
+              <span class="input-group-text" id="inputGroup-sizing-sm">
+                Food Y
+              </span>
+              <input
+                type="number"
+                class="form-control"
+                aria-label="Sizing example input"
+                aria-describedby="inputGroup-sizing-sm"
+                onChange={(e) => setFood(new Node(food.x, e.target.value))}
+              />
+            </div>
+          </div>
+
+          <button
+            className="btn btn-primary"
+            style={{ margin: 6 }}
+            color="red"
+            size="medium"
+            onClick={() => depth_first_search(new Node(1, 1), new Node(3, 2))}
+            variant="outlined"
+          >
+            Depth First Search
+          </button>
+          <button
+            className="btn btn-primary"
+            style={{ margin: 6 }}
+            size="medium"
+            onClick={() => uniform_cost_search(new Node(1, 1), goal_node)}
+            variant="outlined"
+          >
+            Uniform Cost Search
+          </button>
         </div>
-        <button onClick={() => depth_first_search(new Node(1, 1), new Node(3, 2))}>Depth First Search</button>
-        <button onClick={() => uniform_cost_search(new Node(1, 1), goal_node)}>Uniform Cost Search</button>
-      </div>
-    </main>
+      </main>
+    </div>
   );
 }
 
